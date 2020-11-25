8a1c51e5a46c6927994ac2592a95b2fa2e0d9d67
raziEiL <mr.raz4291@gmail.com>
2020-11-25 05:18:44 +0500

    Feat: copy to clipboard

--- src/js/parser.js
+++ src/js/parser.js
@@ -1,26 +1,57 @@
 /**
  * @param {string} text
+ * @returns {(undefined|[string][])}
  */
-const parser = function (text){
+const parseToExcel = function (text) {
     try {
+        if (!text)
+            return;
+
+        text = text.trim();
+
+        if (!text.length)
+            return;
+
         const array = text.trim().split("\n\n");
-        const aoa = [[],[]];
+
+        if (!array.length)
+            return;
+
+        const aoa = [[], []];
         let temp;
 
         for (const string of array) {
             temp = string.split("\n", 1);
-            if (temp.length == 1){
+            if (temp.length == 1) {
                 aoa[0].push(string.replace(temp[0] + "\n", "")); // text
                 aoa[1].push(temp[0]); // 00:00:00:00
             }
-            else{
+            else {
                 aoa[0].push(string);
-                aoa[1].push(" ");
+                aoa[1].push("");
             }
         }
         console.log(aoa);
         return aoa;
-    } catch {}
+    } catch { }
 };
 
-module.exports = parser;
+/**
+ * @param {[string][]} aoa
+ * @returns {(undefined|string)}
+ */
+const parseToBuffer = function (aoa) {
+    if (!aoa || aoa.length !== 2)
+        return;
+    const buffer = [];
+    for (let array of aoa) {
+        array = array.map((s, i) => `"${s}"${i < (array.length - 1) ? "\t" : "\n"}`);
+        buffer.push(...array);
+    }
+    if (buffer.length)
+        return buffer.join("");
+};
+
+module.exports.parseToExcel = parseToExcel;
+module.exports.parseToBuffer = parseToBuffer;
+
